f2645c992209ebb6dea712597b335e7a0e49f99d
Michaela Litherland <malitherland@gmail.com>
2024-10-21 23:38:50 -0400

    created basic Posts page upon selection

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,11 +1,30 @@
-
+import postsData from '/StrictlyPlatonic/postData.json';
 
 export const Posts = () => {
 
     return(
         <>
-        <h1 className="Posts"> Posts!</h1>
-        
+        <div>
+        {postsData.map(post => (
+        <div key={post.id} style={styles.postContainer}>
+          <h3>{post.username}</h3>
+          <p><strong>Interests:</strong> {post.interests.join(', ')}</p>
+          <p>{post.post}</p>
+          <p><strong>Date:</strong> {post.date}</p>
+          <p><strong>Location:</strong> {post.location}</p>
+          <p><strong>Event Planned:</strong> {post.event_planned ? "Yes" : "No"}</p>
+          <div style={styles.commentsContainer}>
+            <h4>Comments:</h4>
+            {post.comments.map((comment, index) => (
+              <div key={index} style={styles.comment}>
+                <p><strong>{comment.username}:</strong> {comment.comment}</p>
+              </div>
+             ))}
+            </div>
+            <hr />
+            </div>
+           ))}
+         </div>
         </>
     )
 
